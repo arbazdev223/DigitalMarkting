6168311cf5fe9fb9bc84ab58dee7a0ba8c16aafd
ifdadevelopment <ifdadevelopment.team@gmail.com>
2025-07-01 17:48:01 +0530

    addChanges

--- src/config.js
+++ src/config.js
@@ -12,7 +12,6 @@ export const FRONTEND_URL = isProduction
   ? import.meta.env.VITE_APP_FRONTEND_PROD
   : import.meta.env.VITE_APP_FRONTEND_DEV;
 
-console.log("ENV:", import.meta.env);
 console.log("BACKEND_URL:", BACKEND_URL);
 console.log("FRONTEND_URL:", FRONTEND_URL);
 

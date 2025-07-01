d47a2232575032fc763af00debe3f153a57ca5e4
ifdadevelopment <ifdadevelopment.team@gmail.com>
2025-07-01 17:27:32 +0530

    addUpdate

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const BACKEND_URL =
   import.meta.env.VITE_APP_PROD_ENV === "production"
     ? (import.meta.env.VITE_APP_BACKEND_PROD?.replace(/\/+$/, "") + "/api")
     : import.meta.env.VITE_APP_BACKEND_DEV || "http://localhost:5000/api";
-
+console.log("BACKEND_URL:", BACKEND_URL);
 export const FRONTEND_URL =
   import.meta.env.VITE_APP_PROD_ENV === "production"
     ? import.meta.env.VITE_APP_FRONTEND_PROD
